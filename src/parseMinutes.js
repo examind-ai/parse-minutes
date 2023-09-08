7524cf489ca80040a9ddc59d19e7c2a67b71a7e3
Johnny Oshika <johnnyoshika@gmail.com>
2023-09-07 21:19:17 -0700

    Refactor parseMinutes for readability

--- src/parseMinutes.js
+++ src/parseMinutes.js
@@ -1,24 +1,29 @@
+const parseTimeRange = sentence => {
+  const regex = /(\d+(\.\d+)?)\s*-\s*(\d+(\.\d+)?)?\s*minute(s)?\.?/g;
+  const match = regex.exec(sentence);
+
+  if (!match) return null;
+
+  const maxMinutes = parseFloat(match[3]);
+  return Math.ceil(maxMinutes);
+};
+
+const parseSingleTime = sentence => {
+  const regex = /(\d+(\.\d+)?)\s*minute(s)?\.?/g;
+  const match = regex.exec(sentence);
+
+  if (!match) return null;
+
+  const minutes = parseFloat(match[1]);
+  return Math.ceil(minutes);
+};
+
 /**
  * Parses the estimated time from a given text.
  * @param {string} sentence - The text to parse the estimated time from.
  * @returns {number|null} - The estimated time in minutes, rounded up to the highest integer, or null if no estimated time was found.
  */
-function parseMinutes(sentence) {
-  const regex = /(\d+(\.\d+)?)\s*-\s*(\d+(\.\d+)?)?\s*minute(s)?\.?/g;
-  const match = regex.exec(sentence);
-  if (match) {
-    const maxMinutes = parseFloat(match[3]);
-    return Math.ceil(maxMinutes);
-  } else {
-    const regex = /(\d+(\.\d+)?)\s*minute(s)?\.?/g;
-    const match = regex.exec(sentence);
-    if (match) {
-      const minutes = parseFloat(match[1]);
-      return Math.ceil(minutes);
-    } else {
-      return null;
-    }
-  }
-}
+const parseMinutes = sentence =>
+  parseTimeRange(sentence) ?? parseSingleTime(sentence);
 
 module.exports = parseMinutes;
